8ae4db8b630852d5df058968bf1cf11f03f5f817
rezomapou <rezomapou@gmail.com>
2026-04-22 20:56:17 -0400

    Update config.js

--- config.js
+++ config.js
@@ -10,10 +10,9 @@ const CONFIG = {
 // DO NOT commit this file with sensitive data to a public repo
 // ============================================================
 
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyEbpXOPhMAyRQVnTBRnMFZb2k5ZG4E0Gq2_-UnJuXnRh2VuuTmFo8Q5A_FynrzBW7n/exec';
-const SHEET_ID   = '1JIsrzuxMn5al6f4ph9TossMtHZufBFxnDylNfL00_w4';
 const ENV        = 'production'; // 'production' or 'dev'
- 
+const SHEET_ID   = '1JIsrzuxMn5al6f4ph9TossMtHZufBFxnDylNfL00_w4';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyEbpXOPhMAyRQVnTBRnMFZb2k5ZG4E0Gq2_-UnJuXnRh2VuuTmFo8Q5A_FynrzBW7n/exec';
   
   TABS: {
     RAW:         "RAW_Registrations",
