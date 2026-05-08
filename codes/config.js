1bc1c0981480b4c1ad0a112921b070569a0b519a
rezomapou <rezomapou@gmail.com>
2026-05-08 17:17:41 -0400

    Update config.js

--- codes/config.js
+++ codes/config.js
@@ -18,7 +18,7 @@ var SITE_CONFIG = SITE_CONFIG || {
 
   // All pages follow: HEADER, TOP_AD, [content], BOTTOM_AD, FOOTER
   PAGES: {
-    index:      { components: ['HEADER', 'FOOTER'] },
+    index:      { components: ['HEADER', 'FEATURES_BLOCK','FOOTER'] },
     register:   { components: ['HEADER', 'REGISTER_BLOCK', 'FOOTER'] },
     contact:    { components: ['HEADER', 'CONTACT_BLOCK',   'FOOTER'] },
     login:      { components: ['HEADER', 'LOGIN_BLOCK',     'FOOTER'] },
